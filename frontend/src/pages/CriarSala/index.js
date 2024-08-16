9aeaa8eceda670d037b9a54350cc53b92f96a8a8
120110190 LIVIA ANIELY DE OLIVEIRA ALMEIDA <livia.almeida@lcc1-05.lcc.ufcg.edu.br>
2024-08-16 09:24:42 -0300

    feat: cria pagina de Criar Sala e adiciona o acesso da rota pelo botao da tela inicial

---
+++ frontend/src/pages/CriarSala/index.js
@@ -0,0 +1,11 @@
+import Menu from '../../components/Menu';
+import './style.css';
+
+export default function CriarSala() {
+    
+    return(
+        <div className="container-criar-sala">
+            <Menu></Menu>
+        </div>
+    );
+}
