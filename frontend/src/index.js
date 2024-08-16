addc6c4d3f9f1092c118bf9ef5c91ec08ad692fd
120110190 LIVIA ANIELY DE OLIVEIRA ALMEIDA <livia.almeida@lcc1-05.lcc.ufcg.edu.br>
2024-08-16 09:34:29 -0300

    config: adiciona biblioteca do prime react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import RoutesApp from './routes';
 
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
