ce6b71991c16cef0084673b01c03e26ccd743fe8
120110190 LIVIA ANIELY DE OLIVEIRA ALMEIDA <livia.almeida@lcc1-52.lcc.ufcg.edu.br>
2024-08-19 14:35:12 -0300

    chore: melhora o design da pagina inicial

--- frontend/src/components/ComoUsar/index.js
+++ frontend/src/components/ComoUsar/index.js
@@ -4,6 +4,10 @@ export default function ComoUsar() {
     return(
         <div className="section-como-usar">
             <h2>COMO USAR</h2>
+            <p>Para usar o nosso sistema, comece criando uma sala no site, inserindo seu nome. 
+                Em seguida, compartilhe o link gerado no grupo de conversa com as pessoas a qual você deseja se reunir. 
+                Os participantes podem acessar o link e votar nos horários que estão disponíveis. 
+                O sistema exibirá os horários mais votados, ajudando a identificar a melhor opção para todos.</p>
         </div>
     );
 }
