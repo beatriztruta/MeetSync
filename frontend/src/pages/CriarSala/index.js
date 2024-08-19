71e6338abb5b458271584b7fcd2ce697a4755c35
120110190 LIVIA ANIELY DE OLIVEIRA ALMEIDA <livia.almeida@lcc1-52.lcc.ufcg.edu.br>
2024-08-19 14:56:46 -0300

    ajusta pagina de Criar Sala

--- frontend/src/pages/CriarSala/index.js
+++ frontend/src/pages/CriarSala/index.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import Menu from '../../components/Menu';
 import './style.css';
-import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+        
 
 export default function CriarSala() {
     
+    const [nome, setNome] = useState('');
+
     return(
         <div className="container-criar-sala">
             <Menu></Menu>
-            <div className="formulario">
-
+            <div className="formulario-criar-sala">
+                
             </div>
         </div>
     );
