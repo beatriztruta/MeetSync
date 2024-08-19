2e75c63a4d9bbfd986c7b140c75b9351bb326465
120110190 LIVIA ANIELY DE OLIVEIRA ALMEIDA <livia.almeida@lcc1-52.lcc.ufcg.edu.br>
2024-08-19 14:43:56 -0300

    config: adiciona primeflex ao projeto

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import RoutesApp from './routes';
 
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'primeflex/primeflex.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
