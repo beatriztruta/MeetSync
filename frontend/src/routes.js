b56bc0713d5a23016065f62fd0dd7934cfe082d0
Aline Brito <aline.neves@ccc.ufcg.edu.br>
2024-08-22 09:07:58 -0300

    Routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PaginaInicial from "./pages/PaginaInicial";
 import CriarSala from "./pages/CriarSala";
+import SalaVotacao from "./pages/SalaVotacao";
 
 export default function RoutesApp(){
     return(
@@ -8,6 +9,7 @@ export default function RoutesApp(){
         <Routes>
             <Route path = '/' element = {<PaginaInicial/>}/>
             <Route path = '/criar-sala' element = {<CriarSala/>}/>
+            <Route path = '/sala-votacao' element = {<SalaVotacao/>}/>
         </Routes>
         
         </BrowserRouter>
