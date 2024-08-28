5a69f94238370f4d0e441a72921c44115c8ca0a9
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-08-27 21:22:31 -0300

    fix: ajusta arquivo de rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,21 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PaginaInicial from "./pages/PaginaInicial";
-<<<<<<< HEAD
 import CriarSala from "./pages/CriarSala";
-=======
->>>>>>> a0af2b2 (config: adiciona o mapeamento de rotas da aplicacao)
 
 export default function RoutesApp(){
     return(
         <BrowserRouter>
         <Routes>
             <Route path = '/' element = {<PaginaInicial/>}/>
-<<<<<<< HEAD
             <Route path = '/criar-sala' element = {<CriarSala/>}/>
-=======
->>>>>>> a0af2b2 (config: adiciona o mapeamento de rotas da aplicacao)
         </Routes>
-        
         </BrowserRouter>
     );
 }
