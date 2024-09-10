1a2d622fc7fa81a9364156fc423a9657ef8f34c8
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-09-09 22:46:22 -0300

    chore: adiciona import do primeicons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import RoutesApp from './routes';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
 import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
