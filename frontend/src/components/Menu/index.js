29c245cc4584d6d26642c88b81dae448acac9b3f
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-09-11 12:34:15 -0300

    style: ajeita alguns estilos do menu

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,59 +1,55 @@
-import './style.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/imgs/logo.png';
 import { Menubar } from 'primereact/menubar';
-import 'primereact/resources/themes/saga-blue/theme.css';  // Opcional, para aplicar o tema
-import 'primereact/resources/primereact.min.css';  // CSS do PrimeReact
-import 'primeicons/primeicons.css';  // Ícones do PrimeReact
-import React, { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import './style.css';
 
 export default function Menu() {
     const [roomId, setRoomId] = useState('');
+    const navigate = useNavigate();
 
     const handleJoinRoom = () => {
         if (roomId.trim()) {
-            window.location.href = `/voting-room/${roomId}`; // Redireciona para a sala de votação com o ID inserido
+            navigate(`/sala-votacao/${roomId}`);
         } else {
             alert('Por favor, insira um ID de sala válido.');
         }
     };
 
-    // Definindo os itens do menu
     const items = [
         {
             label: 'Home',
             icon: 'pi pi-fw pi-home',
-            command: () => { window.location.href = "#home"; }
+            command: () => { navigate('/'); }
         },
         {
             label: 'Sobre',
             icon: 'pi pi-fw pi-info-circle',
-            command: () => { window.location.href = "#about"; }
+            command: () => { navigate('/sobre'); }
         },
-        {
-            label: 'Contato',
-            icon: 'pi pi-fw pi-envelope',
-            command: () => { window.location.href = "#contact"; }
-        }
     ];
 
-    // Template customizado para incluir a logo e o campo de input
-    const start = <img src={logo} alt="Logo da empresa" style={{ marginRight: '10px', height: '40px' }} />;
+    const start = <img src={logo} alt="Logo" style={{ marginRight: '10px', height: '50px' }} />;
     const end = (
         <div className="p-inputgroup">
-            <input 
-                type="text" 
+            <InputText 
                 placeholder="ID da Sala" 
-                className="p-inputtext p-component custom-input" 
-                value={roomId} 
+                className="p-inputtext custom-input" 
                 onChange={(e) => setRoomId(e.target.value)} 
             />
-            <button className="p-button p-component custom-button" onClick={handleJoinRoom}>
+            <button className="p-button custom-button" onClick={handleJoinRoom}>
                 <span className="pi pi-sign-in" />
             </button>
         </div>
     );
 
-    return (
-        <Menubar model={items} start={start} end={end} />
+        return (
+            <Menubar
+            model={items}
+            start={start}
+            end={end}
+            style={{ backgroundColor: '#2f4f4f' }}
+        />
     );  
 };
