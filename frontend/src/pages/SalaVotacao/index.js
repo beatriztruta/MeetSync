c12a4e266b23b9cfa6933c772c57ebc079924ebf
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-09-19 20:41:38 -0300

    chore: ajusta dimensao do menu e icons

--- frontend/src/pages/SalaVotacao/index.js
+++ frontend/src/pages/SalaVotacao/index.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faCog, faShare } from "@fortawesome/free-solid-svg-icons";
 import { InputText } from 'primereact/inputtext';
 import Menu from "../../components/Menu";
 import "primereact/resources/themes/saga-blue/theme.css"; 
@@ -76,86 +74,83 @@ function SalaVotacao() {
   const sortedResultados = resultadosTabela.sort((a, b) => b.totalVotos - a.totalVotos);
 
   return (
-    <div className="flex flex-column align-items-center">
+    <div>
       <Menu />
-      <div
-        className="fundo-desfocado"
-        style={{
-          marginTop: "0.5em",
-          padding: "0.5em",
-          width: "20%",
-          fontSize: "1.5em",
-          fontWeight: "bold",
-          textAlign: "center",
-        }}
-      >
-        Sala de Votação
-      </div>
-      <div
-        className="fundo-desfocado flex flex-column align-items-center w-full xl:w-8 lg:w-6"
-        style={{ margin: "1em", padding: "1em" }}
-      >
-        <form onSubmit={handleVotacao}>
-          <div className="horarios">
-            <label htmlFor="horarios" style={{ textAlign: 'center', color: 'white' }}>Selecione os horários:</label>
-            <div className="cards-container">
-              {horariosDisponiveis.map((horario) => (
-                <div
-                  key={horario.id}
-                  className={`card ${horariosSelecionados.includes(horario.id) ? 'selected' : ''}`}
-                  onClick={() => toggleHorarioSelection(horario.id)}
-                >
-                  <div className="card-content">
-                    <h3>{formatDate(horario.date.split(' ')[0])}</h3>
-                    <p>{horario.time}</p>
+      <div className="flex flex-column align-items-center">
+        <div
+          className="fundo-desfocado"
+          style={{
+            marginTop: "0.5em",
+            padding: "0.5em",
+            width: "20%",
+            fontSize: "1.5em",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Sala de Votação
+        </div>
+        <div
+          className="fundo-desfocado flex flex-column align-items-center w-full xl:w-8 lg:w-6"
+          style={{ margin: "1em", padding: "1em" }}
+        >
+          <form onSubmit={handleVotacao}>
+            <div className="horarios">
+              <label htmlFor="horarios" style={{ textAlign: 'center', color: 'white' }}>Selecione os horários:</label>
+              <div className="cards-container">
+                {horariosDisponiveis.map((horario) => (
+                  <div
+                    key={horario.id}
+                    className={`card ${horariosSelecionados.includes(horario.id) ? 'selected' : ''}`}
+                    onClick={() => toggleHorarioSelection(horario.id)}
+                  >
+                    <div className="card-content">
+                      <h3>{formatDate(horario.date.split(' ')[0])}</h3>
+                      <p>{horario.time}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+            </div>
+
+            <label htmlFor="nome">Nome:</label>
+            <InputText
+              id="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              placeholder="Digite seu nome"
+              required
+              style={{ width: '100%' }} 
+            />
+
+            <div>
+              <button className="vote-btn" style={{ fontWeight: 'bold' }}>Votar</button>
+            </div>
+          </form>
+
+          <div id="resultados">
+            <h2>Resultados</h2>
+              <div className="card">
+                <DataTable value={sortedResultados} tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="horario" header="Horário" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
+                    <Column field="totalVotos" header="Total de Votos" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
+                    <Column field="pessoas" header="Pessoas" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
+                </DataTable>
             </div>
           </div>
-
-          <label htmlFor="nome">Nome:</label>
-          <InputText
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            placeholder="Digite seu nome"
-            required
-            style={{ width: '100%' }} 
-          />
-
-          <div>
-            <button className="vote-btn" style={{ fontWeight: 'bold' }}>Votar</button>
+          <div className="icon-buttons">
+            <button
+              className="icon-button"
+              onClick={handleDelete}
+              style={{ backgroundColor: "grey" }}
+            > 
+              <i className="pi pi-spin pi-trash"/>
+            </button>
           </div>
-        </form>
-
-        <div id="resultados">
-          <h2>Resultados</h2>
-            <div className="card">
-              <DataTable value={sortedResultados} tableStyle={{ minWidth: '50rem' }}>
-                  <Column field="horario" header="Horário" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
-                  <Column field="totalVotos" header="Total de Votos" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
-                  <Column field="pessoas" header="Pessoas" headerStyle={{ background: 'linear-gradient(135deg, #2F4F4F, #4d7979)', color: 'white' }}></Column>
-              </DataTable>
-          </div>
-        </div>
-        <div className="icon-buttons">
-          <button
-            className="icon-button"
-            onClick={handleDelete}
-            style={{ backgroundColor: "grey" }}
-          >
-            <FontAwesomeIcon icon={faTrash} />
-          </button>
-          <button className="icon-button" style={{ backgroundColor: "grey" }}>
-            <FontAwesomeIcon icon={faCog} />
-          </button>
-          <button className="icon-button" style={{ backgroundColor: "grey" }}>
-            <FontAwesomeIcon icon={faShare} />
-          </button>
         </div>
       </div>
     </div>
+
   );
 }
 
