d468d87e7afd88312ab5e12636a052388fc45dad
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-09-22 19:59:43 -0300

    feat: adiciona pagina de Not Found

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import PaginaInicial from "./pages/PaginaInicial";
 import CriarSala from "./pages/CriarSala";
 import SalaVotacao from "./pages/SalaVotacao";
 import PaginaSobre from "./pages/PaginaSobre";
+import NotFound from "./pages/Error";
 
 export default function RoutesApp(){
     return(
@@ -12,6 +13,7 @@ export default function RoutesApp(){
             <Route path = '/criar-sala' element = {<CriarSala/>}/>
             <Route path = '/sala-votacao/:idRoom' element = {<SalaVotacao/>}/>
             <Route path = '/sobre' element = {<PaginaSobre/>}/>
+            <Route path = '*' element = {<NotFound/>}/>
         </Routes>
         </BrowserRouter>
     );
