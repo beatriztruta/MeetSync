1a9b33d14e6668ed2def0a853dd535da8c93e636
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-09-24 19:49:30 -0300

    chore: resolve formatacao do objeto de envio para o post

--- frontend/src/pages/CriarSala/index.js
+++ frontend/src/pages/CriarSala/index.js
@@ -30,7 +30,6 @@ export default function CriarSala() {
     }, [nomeUser]);
 
     const atualizarCampo = (field, value) => {
-      console.log('aqui');
         setSala(prevUser => ({ ...prevUser, [field]: value }));
       };
 
@@ -38,46 +37,36 @@ export default function CriarSala() {
         toast.current.show({severity:'error', summary: 'Erro', detail:'Preencha os campos obrigatórios', life: 3000});
     }
 
-    
-    function formatTimesForPostRoom(times) {
-      const formatted = Object.keys(times).map(dateStr => {
-          return Object.keys(times[dateStr]).map(timeId => {
-              const { startTime, duration } = times[dateStr][timeId];
-              const data = new Date(dateStr);
-
-              console.log(data);
-              console.log(data.toISOString());
-              const start = new Date(dateStr);
-              start.setHours(startTime.code);
-              
-              const end = new Date(start);
-              end.setHours(end.getHours() + parseInt(duration.code));
-
-              return {
-                  date: data,
-                  start: start,
-                  end: end,
-              };
-          });
-      }).flat();
-
-      return formatted;
-  }
-
     const submitData = (sala) => {
         if(isValidValue(sala.name) && isValidValue(sala.title)
         && isValidValue(sala.endingAt) && isValidTimesList(sala.times)) {
-          fetchRoom(sala);
+            const salaPost = {
+                "name": sala.name,
+                "title": sala.title,
+                "description": sala.description? sala.description : "",
+                "times": convertToISOTimezone(sala.times),
+                'endingAt': sala.endingAt
+            };
+            fetchRoom(salaPost);
         } else {
           showError();
         }
 
     }
-    
+
+    function convertToISOTimezone(dates) {
+        return dates.map(dateObj => ({
+            date: new Date(dateObj.date).toISOString(),
+            start: new Date(dateObj.start).toISOString(),
+            end: new Date(dateObj.end).toISOString()
+        }));
+    }
+
     const fetchAndSetRoom = async (sala) => {
       try {
         const idRoom = await fetchRoom(sala);
-
+        console.log("id object:", idRoom); 
+        navigate(`/sala-votacao/${idRoom}`, { state: { isCriador: true, link: 'linkk' } });
       } catch (error) {
         console.error("Erro ao definir a sala:", error);
       }
@@ -86,8 +75,6 @@ export default function CriarSala() {
     async function fetchRoom(sala) {
       try {
         const id = await postRoom(sala); 
-        console.log("id object:", id); 
-        navigate(`/sala-votacao/${id}`, { state: { isCriador: true, link: 'linkk' } });
       } catch (error) {
         console.error("Erro ao criar a sala:", error); 
       }
