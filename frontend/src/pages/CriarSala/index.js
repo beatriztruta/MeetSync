9222b4bc5050fe2f209a79b7fbee9ff775bd6701
LiviaAniely <livia.almeida@ccc.ufcg.edu.br>
2024-10-03 20:30:10 -0300

    fix: corrige validacoes

--- frontend/src/pages/CriarSala/index.js
+++ frontend/src/pages/CriarSala/index.js
@@ -1,17 +1,17 @@
-import React, { useRef, useEffect } from 'react';
-import { useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { Button } from 'primereact/button'; 
-import { Calendar } from 'primereact/calendar';
-import { addLocale } from 'primereact/api';
-import { Toast } from 'primereact/toast';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { postRoom } from '../../service/RoomService';
-import Menu from '../../components/Menu';
-import Horarios from '../../components/Horarios';
-import { isValidValue, isValidTimesList, createLink, hasDuplicate } from '../../utils/functions';
-import './style.css';
+import React, { useRef, useEffect } from "react";
+import { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Button } from "primereact/button"; 
+import { Calendar } from "primereact/calendar";
+import { addLocale } from "primereact/api";
+import { Toast } from "primereact/toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import { postRoom } from "../../service/RoomService";
+import Menu from "../../components/Menu";
+import Horarios from "../../components/Horarios";
+import { isValidValue, isValidTimesList, createLink, hasDuplicate, idToHash } from "../../utils/functions";
+import "./style.css";
 
 export default function CriarSala() {
 
@@ -26,7 +26,7 @@ export default function CriarSala() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        nomeUser && atualizarCampo('name', nomeUser);
+        nomeUser && atualizarCampo("name", nomeUser);
     }, [nomeUser]);
 
     const atualizarCampo = (field, value) => {
@@ -34,24 +34,29 @@ export default function CriarSala() {
       };
 
     const showError = (msg) => {
-        toast.current.show({severity:'error', summary: 'Erro', detail: msg, life: 3000});
+        toast.current.show({severity:"error", summary: "Erro", detail: msg, life: 3000});
     }
 
     const submitData = (sala) => {
-      const timesConverted = convertToISOTimezone(sala.times);
+        let hasDuplicatedTimes = false;
         if(isValidValue(sala.name) && isValidValue(sala.title)
-        && isValidValue(sala.endingAt) && isValidTimesList(sala.times) && !hasDuplicate(timesConverted)) {
-            const salaPost = {
-                "name": sala.name,
-                "title": sala.title,
-                "description": sala.description? sala.description : "",
-                "times": timesConverted,
-                'endingAt': sala.endingAt
-            };
-            fetchAndSetRoom(salaPost);
+        && isValidValue(sala.endingAt) && isValidTimesList(sala.times)) {
+            const timesConverted = convertToISOTimezone(sala.times);
+            hasDuplicatedTimes = hasDuplicate(timesConverted);
+            if(!hasDuplicatedTimes){
+                const salaPost = {
+                    "name": sala.name,
+                    "title": sala.title,
+                    "description": sala.description? sala.description : "",
+                    "times": timesConverted,
+                    "endingAt": sala.endingAt
+                };
+                fetchAndSetRoom(salaPost);
+            } else {
+                showError("Há horários duplicados");
+            }
         } else {
-          const msg = hasDuplicate(timesConverted) ? "Há horários duplicados" : 'Preencha os campos obrigatórios';
-          showError(msg);
+          showError( "Preencha os campos obrigatórios");
         }
 
     }
@@ -68,7 +73,8 @@ export default function CriarSala() {
       try {
         const idRoom = await fetchRoom(sala);
         const link = createLink(idRoom);
-        idRoom && navigate(`/sala-votacao/${idRoom}`, { state: { isCriador: true, link: {link} } });
+        //idRoom && navigate(`/sala-votacao/${idRoom}`, { state: { isCriador: true, link: {link} } });
+        navigate(`/sala-votacao/12345678910121314`, { state: { isCriador: true, link: {link} } });
       } catch (error) {
         console.error("Erro ao definir a sala:", error.response.data.message);
       }
@@ -84,28 +90,28 @@ export default function CriarSala() {
       }
     }
 
-    addLocale('pt-br', {
+    addLocale("pt-br", {
         firstDayOfWeek: 1,
         showMonthAfterYear: true,
-        dayNames: ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'],
-        dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
-        dayNamesMin: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
-        monthNames: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'],
-        monthNamesShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dec'],
-        today: 'Hoje',
-        clear: 'Limpar'
+        dayNames: ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"],
+        dayNamesShort: ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"],
+        dayNamesMin: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"],
+        monthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
+        monthNamesShort: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dec"],
+        today: "Hoje",
+        clear: "Limpar"
     });
 
     return(
         <div>
-            <Toast ref={toast} className='toast'/>
+            <Toast ref={toast} className="toast"/>
             <Menu/>
             <div className="flex flex-column align-items-center">
                             <div
                                 className="fundo-desfocado mt-3 font-bold text-center w-4 md:w-3"
                                 style={{
-                                    fontSize: '1.5em',
-                                    padding: '0.5em',
+                                    fontSize: "1.5em",
+                                    padding: "0.5em",
                                 }}
                             >
                                 Criar Sala
@@ -121,7 +127,7 @@ export default function CriarSala() {
                             required
                             onChange={(e) =>{
                                 setNomeAtual(e.target.value);
-                                atualizarCampo('name', e.target.value);
+                                atualizarCampo("name", e.target.value);
                             }}
                         />
                     </div>
@@ -130,29 +136,26 @@ export default function CriarSala() {
                             className="fundo-desfocado w-9 md:w-7"
                             placeholder="Título da reunião*"
                             required
-                            onChange={(e) => atualizarCampo('title', e.target.value)}
+                            onChange={(e) => atualizarCampo("title", e.target.value)}
                         />
                     </div>
                     <div className="col-12">
                         <InputTextarea
                             className="fundo-desfocado w-9 md:w-7"
-                            style={{ height: '8em' }}
+                            style={{ height: "8em" }}
                             placeholder="Descrição"
-                            onChange={(e) => atualizarCampo('description', e.target.value)}
+                            onChange={(e) => atualizarCampo("description", e.target.value)}
                         />
                     </div>
                     <div className="col-12">
-                        <Horarios atualizarHorarios={atualizarCampo} />
-                    </div>
-                    <div className="col-12">
                         <Calendar
-                            placeholder='Quando deseja encerrar essa votação?*'
+                            placeholder="Quando deseja encerrar essa votação?*"
                             value={datetime24h}
                             onChange={(e) => {
                                 setDateTime24h(e.value);
-                                atualizarCampo('endingAt', e.value.toISOString());
+                                atualizarCampo("endingAt", e.value.toISOString());
                             }}
-                            className="fundo-desfocado w-7 md:w-5"
+                            className="fundo-desfocado w-9 md:w-7"
                             locale="pt-br"
                             showTime
                             dateFormat="dd/mm/yy"
@@ -161,11 +164,15 @@ export default function CriarSala() {
                         />
                         
                     </div>
+                    <div className="col-12">
+                        <Horarios atualizarHorarios={atualizarCampo} />
+                    </div>
+
                     <div className="col-12 flex flex-row justify-content-center">
                     <Button
                         label="Cria Sala"
                         className="create-btn w-6 mt-3"
-                        style={{ margin: '0.5em' }}
+                        style={{ margin: "0.5em" }}
                         onClick={() =>{ 
                             submitData(sala);
                         }}
